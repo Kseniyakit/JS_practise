f4aa9ce1a7749ed688118e0fbda8d6284bb61d10
Kseniya <kseniyakit@gmail.com>
2020-02-16 13:55:12 +0200

    commit9

--- Stud/src/HW1/arrays/task6.js
+++ Stud/src/HW1/arrays/task6.js
@@ -1,9 +1,26 @@
 /* const array = [2, 78, 56, 45]; */
 
-function arrayReversed(arr) {
+/* function arrayReversed(arr) {
     if (!Array.isArray(arr) || (arr === null) || (arr === undefined) || (isNaN(Array.isArray(arr)))) {
         return null;
     }
     return arr.reverse();
 
-};
+}; */
+let a = [1, 2, 3];
+function arrayReversed (arr){
+    if (!Array.isArray(arr) || (arr === null) || (arr === undefined) || (isNaN(Array.isArray(arr)))) {
+        return null;
+    }
+       let array = [];
+   
+       for( let i = arr.length; i--; ){
+   
+           array.push( arr[i] );
+   
+       };
+  
+       return array;
+   
+    };
+    
